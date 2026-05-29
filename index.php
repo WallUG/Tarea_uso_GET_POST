8f82d68481156ede651ed92d6e703b6dcb644bd3
MavenW <ytsiuioh45@gmail.com>
2026-05-28 19:50:01 -0500

    Primeros pasos php

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+echo "Hello, World!";
+
+echo "<h1>Welcome to Moodle!</h1>";
+echo "<p>This is the index page of our Moodle plugin.</p>";
+
+//todas las variables enviezan con $ y se pueden usar para almacenar datos, como texto, números o resultados de funciones.
+$message = "This is a message stored in a variable.";
+echo $message; // Imprime el contenido de la variable $message
+
+
+?>
